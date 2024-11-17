ef4f8dc7f6d7a5be8eea463db42c10445f4091f9
Akankshavdixit <131232617+Akankshavdixit@users.noreply.github.com>
2024-11-18 00:01:43 +0530

    deployment successful

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,29 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const predictroutes = require('./routes/predict');
 
-const predictroutes = require('./routes/predict')
 
 app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
+    origin: 'https://heart-disease-prediction-73fg.vercel.app', 
+    //origin : 'http://localhost:3000'
+    credentials: true, 
 }));
-const port = 8000
+
+const port = 8000;
 app.use(express.json());
+
 
 app.post('/predict', predictroutes);
 
+
+app.options('/predict', cors({
+    origin: 'https://heart-disease-prediction-73fg.vercel.app',
+    credentials: true
+}));
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-})
+});
